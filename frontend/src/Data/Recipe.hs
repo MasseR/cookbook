e140d2e8e0cfb234bbd742d9dc07bf32bb21cd60
Mats Rauhala <mats.rauhala@iki.fi>
2020-01-19 13:32:53 +0200

    Semigroup instance for recipe

--- frontend/src/Data/Recipe.hs
+++ frontend/src/Data/Recipe.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Data.Recipe where
 
-data Recipe a
-  = Recipe { ingredients :: [a]
+-- Try to model the data
+
+data Recipe m a
+  = Recipe { ingredients  :: m a
            , instructions :: Text
            }
+
+instance (Semigroup (m a)) => Semigroup (Recipe m a) where
+  Recipe a b <> Recipe a' b' = Recipe (a <> a') (b <> b')
