236f5529bef2fdaf41fbac53d1153c5dd16afd5f
Mats Rauhala <mats.rauhala@iki.fi>
2020-01-19 15:41:53 +0200

    Play around with lenses and views

--- frontend/src/Data/Name.hs
+++ frontend/src/Data/Name.hs
@@ -11,11 +11,17 @@ A name and its lens
 -}
 module Data.Name where
 
-import Control.Lens (Lens', lens, view, set)
+import Control.Lens (Lens', lens, view, set, iso, Iso')
 
 -- | Newtype over name
-newtype Name = Name Text deriving (Eq, Ord, Show)
+newtype Name = Name Text
+  deriving (Eq, Ord, Show)
 
+-- | Isomorphism between 'Name' and 'Text'
+_Name :: Iso' Name Text
+_Name = iso (\(Name n) -> n) Name
+
+-- | Lenses over the name
 class HasName a where
   getName :: a -> Name
   getName = view name
--- frontend/src/MyLib.hs
+++ frontend/src/MyLib.hs
@@ -5,13 +5,25 @@ module MyLib (defaultMain) where
 import           Miso
 import           Miso.String
 
-import           Control.Lens (Lens', lens, (+=), (-=), (^.))
+import           Control.Lens  (Lens', lens, (+=), (-=), (^.), _Just, iso, Iso', from)
+import           Data.Name     (name, _Name)
 
-data Model = Model { _counter :: Int }
+import           Data.Cookbook (Food)
+
+_MisoString :: ToMisoString a => Iso' MisoString a
+_MisoString = iso fromMisoString toMisoString
+
+data Model
+  = Model { _counter :: Int
+          , _food    :: Maybe Food
+          }
            deriving (Eq)
 
 counter :: Lens' Model Int
 counter = lens _counter (\m x -> m{_counter=x})
+
+food :: Lens' Model (Maybe Food)
+food = lens _food (\m x -> m{_food=x})
 
 data Action
   = AddOne
@@ -24,7 +36,7 @@ defaultMain :: IO ()
 defaultMain = startApp App {..}
   where
     initialAction = SayHelloWorld
-    model = Model 0
+    model = Model 0 Nothing
     update = fromTransition . updateModel
     view = viewModel
     events = defaultEvents
@@ -47,8 +59,20 @@ viewModel x = section_ [class_ "section"] [
       , button_ [ onClick AddOne ] [ text "+" ]
       , text . ms $ x ^. counter
       , button_ [ onClick SubtractOne ] [ text "-" ]
+      , div_ [class_ "columns"] [ viewFood (x ^. food) ]
       ]
   ]
   where
     bulma = "https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"
     fontawesome = "https://use.fontawesome.com/releases/v5.3.1/js/all.js"
+
+viewFood :: Maybe Food -> View Action
+viewFood f = div_ [ class_ "column is-two-thirds" ] [
+    textField "Name" (f ^. _Just . name . _Name . from _MisoString)
+  ]
+  where
+    textField = field "text"
+    field fieldType lbl  val = div_ [ class_ "field" ] [
+        label_ [ class_ "label" ] [ text lbl ]
+      , div_ [ class_ "control" ] [ input_ [ class_ "input", type_ fieldType, value_ val] ]
+      ]
